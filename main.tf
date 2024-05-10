e1b965e6305cf1d7a2578111603b744a9a631f18
elicarter <89507487+elicarter@users.noreply.github.com>
2024-05-10 09:14:37 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
+provider "google" {
+  project = "acme-tfc-demo" 
+  region  = "us-west3"
+}
 
+
+resource "google_compute_instance" "vm_instance" {
+  count         = var.instance_count
+  name          = "acmewebserver-${count.index + 1}"
+  machine_type = var.gcp_instance_type
+  zone         = "us-west3-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
